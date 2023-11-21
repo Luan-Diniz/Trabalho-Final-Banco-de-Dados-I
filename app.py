9367d9516ef3eda5862e0ca32d8b86b37701daf9
Luan-Diniz <luan.diniz.moraes@hotmail.com>
2023-11-21 14:18:51 -0300

    main loop comentado

--- app.py
+++ app.py
@@ -6,7 +6,33 @@ connection = psycopg2.connect(host="localhost", dbname="formula1",
 cursor = connection.cursor()
 
 
-#CODE HERE
+
+isRunning = True
+
+'''
+while isRunning:
+	#CODE HERE
+	print("----------BANCO DE DADOS DA FORMULA1 ---------------")
+	print("Selecione o que você quer fazer: ")
+	print("\tCONSULTA: Digite 1")
+	print("\tOPERAÇÃO EM UMA TABELA: Digite 2")
+	print("\tSAIR DO PROGRAMA: Digite 0")
+	print()
+
+	user_input = input()
+	if user_input == '0':
+		isRunning = False
+		continue
+	elif user_input == '1':
+		pass
+	elif user_input == '2':
+		pass
+	else:
+		print("OPERAÇÃO INVÁLIDA!")
+'''
+
+
+'''
 teste = input("Type 0 to create a table Ararangua")
 
 if teste == "0":
@@ -15,6 +41,7 @@ if teste == "0":
             name VARCHAR(255)
         );
     """)
+'''
 
 connection.commit()
 connection.close()
