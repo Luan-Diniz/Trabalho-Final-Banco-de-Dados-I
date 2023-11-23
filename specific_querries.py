2d3c04981bb0c06f87236a53d25676d0ace586dc
Luan-Diniz <luan.diniz.moraes@hotmail.com>
2023-11-23 15:40:49 -0300

    comentarios no specific_querries.py

--- specific_querries.py
+++ specific_querries.py
@@ -7,7 +7,23 @@ class SpecificQuerries:
     	pass
     	
     def run_querry2():
+        #Ordenação dos pilotos baseados em seus pontos? Coluna com função de agregação
     	pass
+        
     
     def run_querry3():
+        #LEFT JOIN Aqui
+        #CRIAR Pilotos que não estão associados a nenhuma equipe
+        #Essa pega os pilotos que não estão associados a nenhuma equipe
     	pass
+
+
+'''
+-3 Consultas envolvendo 2 ou + tabelas. (Devem projetar no mínimo 2 colunas)
+-Duas consultas devem ter uma das colunas com uma função de agregação.
+-A última deve usar left ou right join
+
+
+!!! Por amostras dos resultados no relatório
+
+'''
