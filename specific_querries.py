1396b4104feac30abfc1df35eb124bf0d954b625
Luan-Diniz <luan.diniz.moraes@hotmail.com>
2023-11-23 13:44:42 -0300

    arquivo para as querries personalizadas

---
+++ specific_querries.py
@@ -0,0 +1,13 @@
+import psycopg2
+class SpecificQuerries:
+    def __init__(db_cursor):
+        self.con = db_cursor
+        
+    def run_querry1():
+    	pass
+    	
+    def run_querry2():
+    	pass
+    
+    def run_querry3():
+    	pass
