540ee35494d880f10b0fa75e520c31254375bae4
Isac Martins <isacmartins.223@gmail.com>
2023-11-27 17:32:02 -0300

    rework and creating crude manager

--- app.py
+++ app.py
@@ -2,59 +2,92 @@ import psycopg2
 import pandas as pd
 from specific_queries import SpecificQueries
 
-connection = psycopg2.connect(host="localhost", dbname="formula1",
-		user="postgres", password="postgres", port=2023)
 
-cursor = connection.cursor()
-our_queries = SpecificQueries(cursor)
-
-
-isRunning = True
-
-while isRunning:
-	print("----------BANCO DE DADOS DA FORMULA1 ---------------")
-	print("Selecione o que você quer fazer: ")
-	print("\tCONSULTA: Digite 1")
-	print("\tOPERAÇÃO EM UMA TABELA: Digite 2")
-	print("\tSAIR DO PROGRAMA: Digite 0")
-	print()
-
-	user_input = input()
-	if user_input == '0':
-		isRunning = False
-		print("Fechando o programa...")
-		continue
-
-	elif user_input == '1':
-		print("Qual das seguintes consultas você quer fazer?")
-		print("\t CLASSIFICAÇÃO PILOTOS: Digite 1")
-		print("\t PATROCINADORES DAS EQUIPES: Digite 2")
-		print("\t CIRCUITOS NÃO USADOS: Digite 3")
-		user_input = input()
-
-		if user_input == '1':
-			ano_consulta = input("De qual ano?")
-			print()
-			our_queries.show_pontuation(ano_consulta)
-
-		elif user_input == '2':
-			our_queries.not_in_team()
-
-		elif user_input == '3':
-			our_queries.not_used_tracks()
-		
-		else:
-			print("Inválido")
-			print()
-                
-
-	elif user_input == '2':
-		print("Em qual tabela?")
-		#TODO
-	else:
-		print("OPERAÇÃO INVÁLIDA!")
+def connect_to_db():
+    return psycopg2.connect(
+        host="localhost",
+        dbname="formula1",
+        user="postgres",
+        password="postgres",
+        port=2023,
+    )
 
 
+def get_user_choice(prompt, options):
+    while True:
+        print(prompt)
+        for option, description in options.items():
+            print(f"\t{description}: Digite {option}")
+        choice = input()
+        if choice in options:
+            return choice
+        else:
+            print("Opção inválida. Tente novamente.\n")
 
-connection.commit()
-connection.close()
+
+def main():
+    connection = connect_to_db()
+    cursor = connection.cursor()
+    queries = SpecificQueries(cursor)
+
+    while True:
+        main_choice = get_user_choice(
+            "----------BANCO DE DADOS DA FORMULA1 ---------------\nSelecione o que você quer fazer:",
+            {"1": "CONSULTA", "2": "OPERAÇÃO EM UMA TABELA", "0": "SAIR DO PROGRAMA"},
+        )
+
+        if main_choice == "0":
+            print("Fechando o programa...")
+            break
+
+        elif main_choice == "1":
+            query_choice = get_user_choice(
+                "Qual das seguintes consultas você quer fazer?",
+                {
+                    "1": "CLASSIFICAÇÃO PILOTOS",
+                    "2": "PATROCINADORES DAS EQUIPES",
+                    "3": "CIRCUITOS NÃO USADOS",
+                },
+            )
+
+            if query_choice == "1":
+                ano_consulta = input("De qual ano? ")
+                queries.show_pontuation(ano_consulta)
+
+            elif query_choice == "2":
+                queries.not_in_team()
+
+            elif query_choice == "3":
+                queries.not_used_tracks()
+
+        elif main_choice == "2":
+            user_choice = get_user_choice(
+                "O que você quer fazer?",
+                {
+                    "1": "INSERIR",
+                    "2": "ATUALIZAR",
+                    "3": "DELETAR",
+                    "4": "CONSULTAR",
+                },
+            )
+            table_choice = get_user_choice(
+                "Escolha a tabela para operar:",
+                {
+                    "1": "Transmissão",
+                    "2": "Circuito",
+                    "3": "Patrocinadores",
+                    "4": "Campeonato",
+                    "5": "Equipe",
+                    "6": "Piloto",
+                    "7": "Carro",
+                    "8": "Transmite",
+                    "9": "Participa",
+                    "10": "Possui",
+                },
+            )
+    connection.commit()
+    connection.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ db_manager.py
@@ -0,0 +1,84 @@
+from enum import Enum
+from crude_operator import CrudeOperator
+
+
+class TableOption(Enum):
+    TRANSMISSAO = 1
+    CIRCUITO = 2
+    PATROCINADORES = 3
+    CAMPEONATO = 4
+    EQUIPE = 5
+    PILOTO = 6
+    CARRO = 7
+    TRANSMITE = 8
+    PARTICIPA = 9
+    POSSUI = 10
+
+
+class UserOption(Enum):
+    INSERT = 1
+    UPDATE = 2
+    DELETE = 3
+    SELECT = 4
+
+
+class DBManager:
+    @staticmethod
+    def execute_user_action(user_option, table_option, db_cursor):
+        operator = CrudeOperator(db_cursor)
+        action = DBManager.get_action(user_option)
+        return action(table_option, operator) if action else "Opção inválida"
+
+    @staticmethod
+    def get_action(user_option):
+        return {
+            UserOption.INSERT: DBManager.perform_insert,
+            UserOption.UPDATE: DBManager.perform_update,
+            UserOption.DELETE: DBManager.perform_delete,
+            UserOption.SELECT: DBManager.perform_select,
+        }.get(user_option)
+
+    @staticmethod
+    def perform_insert(table_option, operator):
+        table_name = DBManager.get_table_name(table_option)
+        if table_name:
+            # Insert logic here
+            return f"Insert into {table_name}"
+        return "Opção inválida"
+
+    @staticmethod
+    def perform_update(table_option, operator):
+        # Update logic
+        ...
+
+    @staticmethod
+    def perform_delete(table_option, operator):
+        # Delete logic
+        ...
+
+    @staticmethod
+    def perform_select(table_option, operator):
+        # Select logic
+        ...
+
+    @staticmethod
+    def get_table_name(table_option):
+        return {
+            TableOption.TRANSMISSAO: "Transmissão",
+            TableOption.CIRCUITO: "Circuito",
+            TableOption.PATROCINADORES: "Patrocinadores",
+            TableOption.CAMPEONATO: "Campeonato",
+            TableOption.EQUIPE: "Equipe",
+            TableOption.PILOTO: "Piloto",
+            TableOption.CARRO: "Carro",
+            TableOption.TRANSMITE: "Transmite",
+            TableOption.PARTICIPA: "Participa",
+            TableOption.POSSUI: "Possui",
+        }.get(table_option)
+
+
+# Example Usage
+db_cursor = ...
+user_option = UserOption.INSERT
+table_option = TableOption.CIRCUITO
+result = DBManager.execute_user_action(user_option, table_option, db_cursor)
